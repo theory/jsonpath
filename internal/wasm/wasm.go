4fdb132ea67ec774e47ef8bce7bba292586d2e7f
David E. Wheeler <david@justatheory.com>
2024-11-04 20:28:29 -0500

    Test wasm/main.go
    Remove error checking, since the inputs are hard-coded and the outputs
    expected. Then just call `main()` in a test. Basic coverage to ensure it
    compiles and runs.
    
    While at it, ignore the `pub` directory created by the playground
    branch.

--- internal/wasm/wasm.go
+++ internal/wasm/wasm.go
@@ -4,26 +4,21 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 
 	"github.com/theory/jsonpath"
 )
 
 func main() {
 	// Parse a jsonpath query.
-	p, err := jsonpath.Parse(`$.foo`)
-	if err != nil {
-		log.Fatal(err)
-	}
+	p, _ := jsonpath.Parse(`$.foo`)
 
 	// Select values from unmarshaled JSON input.
 	result := p.Select([]byte(`{"foo": "bar"}`))
 
 	// Show the result.
-	items, err := json.Marshal(result)
-	if err != nil {
-		log.Fatal(err)
-	}
+	//nolint:errchkjson
+	items, _ := json.Marshal(result)
+
 	//nolint:forbidigo
 	fmt.Printf("%s\n", items)
 }
